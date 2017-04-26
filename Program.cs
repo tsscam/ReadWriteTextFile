064d5871c3b520a400a6c75a63b36e5158e4febd
Theresa Skonieczny <tsscamaro@gmail.com>
2017-04-26 10:43:15 -0400

    TEST FILE FOR TXT FILE READ/WRITE

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Please input a message");
+            string input = Console.ReadLine();
+            WriteToFile(input);
+
+        }
+
+        public static void WriteToFile(string input)
+        {
+
+            StreamWriter sw = new StreamWriter("../../TextFile1.txt", true);
+            sw.WriteLine(input);
+
+            sw.Close();
+        }
+    }
+}
+
